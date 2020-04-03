c00b67dd0090a7a6ebc4df8d8f31e7322e735e1c
zzy <zhangzhanyong@julive.com>
2020-04-03 23:29:19 +0800

    【init project】

---
+++ lib/flutter_dynamic_form.dart
@@ -0,0 +1,10 @@
+library flutter_dynamic_form;
+
+import 'package:flutter/cupertino.dart';
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
+
